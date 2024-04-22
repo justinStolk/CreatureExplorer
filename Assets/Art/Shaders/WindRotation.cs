300efcb6b7db3be8a80ef96605f359c480bf7cae
SyllverS <ajaniek@live.nl>
2024-04-22 15:57:30 +0200

    Added wind controller and set normal maps to strength 1

---
+++ Assets/Art/Shaders/WindRotation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WindRotation : MonoBehaviour
+{
+    //public Transform WindController;
+    public Transform child;
+    float Rotation;
+    float Movement;
+    public float WindStrength = 1f;
+    Transform WindController;
+
+    void Start()
+    {
+        Vector3 worldPosition = transform.position;
+        child = this.gameObject.transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        Transform WindController = GameObject.FindWithTag("WindController").transform;
+        Rotation = WindController.eulerAngles.y;
+        Debug.Log(Rotation);
+        Debug.Log(Movement);
+        Movement = (Mathf.Cos(Time.time)+2) * WindStrength;
+        transform.rotation = Quaternion.Euler(Movement, Rotation, 0);
+        child.transform.rotation = Quaternion.Euler((Movement*1.5f), Rotation, 0);
+
+    }
+}
