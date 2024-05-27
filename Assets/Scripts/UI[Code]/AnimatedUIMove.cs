ffb714addcdc18c9fc0e7df0c30e2d2a0c59c76e
Inger <inger.smit1@gmail.com>
2024-05-27 13:33:14 +0200

    Added flatscreen version of stickersheet

---
+++ Assets/Scripts/UI[Code]/AnimatedUIMove.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedUIMove : MonoBehaviour
+{
+    [SerializeField] private Vector3 inPosition, outPosition;
+
+    [SerializeField] private float moveDuration;
+    [SerializeField] private AnimationCurve moveCurve;
+
+    [SerializeField] private bool unfurled;
+
+    private Transform targetTransform;
+
+    private void Start()
+    {
+        if (targetTransform == null)
+        {
+            targetTransform = transform;
+        }
+    }
+
+    public void ToggleMove()
+    {
+        StopAllCoroutines();
+
+        if (unfurled)
+            StartCoroutine(Move(targetTransform.localPosition, inPosition));
+        else
+            StartCoroutine(Move(targetTransform.localPosition, outPosition));
+
+        unfurled = !unfurled;
+    }
+
+    private IEnumerator Move(Vector3 from, Vector3 to)
+    {
+        float timer = 0;
+
+        while (timer <= moveDuration)
+        {
+            targetTransform.localPosition = Vector3.Lerp(from, to, moveCurve.Evaluate(timer / moveDuration));
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        targetTransform.localPosition = to;
+    }
+}
