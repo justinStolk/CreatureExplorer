0f1c4d8a0253555dc05eb1f59450a255058cba7b
iClaimThisName <rebrupert@gmail.com>
2024-06-06 12:07:39 +0200

    Fucked everything

--- Assets/Rebecca Grad/ChargerCreature.cs
+++ Assets/Rebecca Grad/ChargerCreature.cs
@@ -7,6 +7,8 @@ public class ChargerCreature : MonoBehaviour
 {
     public GameObject Track_walk_gameObject;
     public GameObject Track_ramTree_gameObject;
+    public GameObject BerrySprite;
+    new Vector3 BerrySpawnPoint;
     Cinemachine.CinemachinePathBase Track_walk;
     Cinemachine.CinemachinePathBase Track_ramTree;
     public GameObject DollyCart_Charger;
@@ -27,6 +29,7 @@ public class ChargerCreature : MonoBehaviour
         Track_walk = Track_walk_gameObject.GetComponent<CinemachinePathBase>();
         Track_ramTree = Track_ramTree_gameObject.GetComponent<CinemachinePathBase>();
         anim = GetComponent<Animator>();
+        BerrySpawnPoint = new Vector3(BerrySprite.position.x, BerrySprite.position.y, BerrySprite.position.z);
     }
 
     // Update is called once per frame
@@ -141,6 +144,7 @@ public class ChargerCreature : MonoBehaviour
         {
             if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f)
             {
+                BerrySprite.GetComponent<Rigidbody>().useGravity = true;
                 DollyCart_Charger.GetComponent<CinemachineDollyCart>().m_Speed = 0;
                 AnimationPlayOnce("Eat");
                 // add berry that falls from the tree at this point, then a small delay before the animation happens
@@ -149,6 +153,8 @@ public class ChargerCreature : MonoBehaviour
 
             if (anim.GetCurrentAnimatorStateInfo(0).IsName("Eat") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f)
             {
+                BerrySprite.GetComponent<Rigidbody>().useGravity = false;
+                BerrySprite.transform.position(BerrySpawnPoint);
                 DollyCart_Charger.GetComponent<CinemachineDollyCart>().m_Speed = 3;
                 AnimationLoop("Walk");
             }
--- Assets/Rebecca Grad/LeafCreature.cs
+++ Assets/Rebecca Grad/LeafCreature.cs
@@ -3,22 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
+
+[RequireComponent(typeof(Animator))]
 public class LeafCreature : MonoBehaviour
 {
     public GameObject Track_walk_gameObject;
     Cinemachine.CinemachinePathBase Track_walk;
     public GameObject DollyCart_Leaf;
-    private Animator anim;
 
     Cinemachine.CinemachinePathBase Track_current_check;
     float CartPosition_check;
     float CartSpeed_check;
 
+    // New boolean hell <3
+    private Animator anim;
+    public UnityAnimationEvent OnAnimationStart;
+    public UnityAnimationEvent OnAnimationComplete;
+    public float waitTime = 5.0f;
+    public float timer = 0.0f;
+    public List<GameObject> points = new List<GameObject>();
+    public int sequence = -1;
+    public List<string> sequences = new List<string>();
+    public float sequenceWait = 0.0f;
+    public float sequenceWaitTimer = 0.0f;
+    public int animationToRun = -1;
+    public int lastAnimationRun = -1;
+    public int currentAnimationLoop = 0;
+    public int animationLoops = 0;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        for (int i = 0; i < anim.runtimeAnimatorController.animationClips.Length; i++)
+        {
+            AnimationClip clip = anim.runtimeAnimatorController.animationClips[i];
+
+            AnimationEvent animationStartEvent = new AnimationEvent();
+            animationStartEvent.time = 0;
+            animationStartEvent.functionName = "AnimationStartHandler";
+            animationStartEvent.stringParameter = clip.name;
+
+            AnimationEvent animationEndEvent = new AnimationEvent();
+            animationEndEvent.time = clip.length;
+            animationEndEvent.functionName = "AnimationCompleteHandler";
+            animationEndEvent.stringParameter = clip.name;
+
+            clip.AddEvent(animationStartEvent);
+            clip.AddEvent(animationEndEvent);
+        }
+    }
+
+    public void AnimationStartHandler(string name)
+    {
+        // Debug.Log($"{name} animation start.");
+        OnAnimationStart?.Invoke(name);
+    }
+    public void AnimationCompleteHandler(string name)
+    {
+        sequenceWaitTimer = 0.0f;
+        // Debug.Log($"{name} animation complete.");
+        OnAnimationComplete?.Invoke(name);
+
+        if (sequences.Count > 0)
+        {
+            if (currentAnimationLoop >= animationLoops)
+            {
+                sequence++;
+                currentAnimationLoop = 0;
+                animationLoops = 0;
+            }
+            else
+            {
+                currentAnimationLoop++;
+            }
+        }
+
+        if (sequence >= sequences.Count)
+        {
+            lastAnimationRun = animationToRun;
+            animationToRun = -1;
+            sequence = -1;
+            sequences.Clear();
+            sequenceWait = 0.0f;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Track_walk = Track_walk_gameObject.GetComponent<CinemachinePathBase>();
-        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,16 +102,96 @@ public class LeafCreature : MonoBehaviour
         CartPosition_check = DollyCart_Leaf.GetComponent<CinemachineDollyCart>().m_Position;
         CartSpeed_check = DollyCart_Leaf.GetComponent<CinemachineDollyCart>().m_Speed;
 
-        if (Track_current_check == Track_walk)
+        if (sequences.Count == 0 && sequence < 0 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f)
         {
-            if (CartSpeed_check > 0)
+            timer += Time.deltaTime;
+
+            if (timer > waitTime)
             {
-                AnimationLoop("Walk");
+                timer = 0.0f;
+
+                while (animationToRun == -1 || animationToRun == lastAnimationRun)
+                {
+                    animationToRun = Random.Range(0, points.Count);
+                }
             }
-            else
-            {
-                AnimationLoop("Neutral");
-            }
+        }
+
+        if (animationToRun >= 0 && animationToRun <= points.Count)
+        {
+            Behaviour(points[animationToRun]);
+        }
+    }
+
+    public bool IsAnimationReady()
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f && sequenceWaitTimer >= sequenceWait;
+    }
+
+    void Behaviour(GameObject startLocation)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f && sequenceWaitTimer < sequenceWait)
+        {
+            sequenceWaitTimer += Time.deltaTime;
+        }
+
+        switch (animationToRun)
+        {
+            case 0:
+                if (sequences.Count == 0)
+                {
+                    sequences.Add("Walk");
+                    sequence++;
+                }
+
+                if (IsAnimationReady())
+                {
+                    switch (sequence)
+                    {
+                        case 0:
+                            //DollyCart_Leaf.GetComponent<CinemachineDollyCart>().m_Position = 0;
+                            //this.transform.position = DollyCart_Leaf.GetComponent<CinemachineDollyCart>().transform.position;
+                            DollyCart_Leaf.GetComponent<CinemachineDollyCart>().m_Speed = 3;
+
+                            AnimationLoop(sequences[0]);
+
+                            break;
+
+                        /*case 1:
+                            DollyCart_Stoffer.GetComponent<CinemachineDollyCart>().m_Speed = 5;
+                            AnimationLoop(sequences[1]);
+                            animationLoops = 5;
+                            break;
+
+                        case 2:
+                            DollyCart_Stoffer.GetComponent<CinemachineDollyCart>().m_Speed = 0;
+                            AnimationPlayOnce(sequences[2]);
+                            break;*/
+                    }
+                }
+
+                return;
+
+            /*case 0:
+            case 2:
+            case 3:
+            case 4:
+                this.transform.position = new Vector3(popUpLocation.transform.position.x, popUpLocation.transform.position.y, popUpLocation.transform.position.z);
+
+                if (sequences.Count == 0)
+                {
+                    sequences.Add("Up");
+                    sequences.Add("Down");
+                    sequenceWait = 3.0f;
+                    sequence++;
+                }
+
+                if (IsAnimationReady())
+                {
+                    AnimationPlayOnce(sequences[sequence]);
+                }
+
+                break;*/
         }
     }
 
